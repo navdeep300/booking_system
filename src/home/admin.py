0eb26f203d4df4d96c2c06616e0a5db7bf557512
mandeeps708 <mandeeps708@gmail.com>
2015-02-09 01:03:42 +0530

    admin list_display

--- src/home/admin.py
+++ src/home/admin.py
@@ -4,12 +4,14 @@ from .models import Booking, Hall
 # Register your models here.
 
 class BookingAdmin(admin.ModelAdmin):
+	list_display = ['hall', 'event_name', 'name', 'date', 'start_time', 'end_time', 'email', 'status' ]
 	class Meta:
 		model = Booking
 
 admin.site.register(Booking, BookingAdmin)
 
 class HallAdmin(admin.ModelAdmin):
+	list_display = ['hall', 'seats']
 	class Meta:
 		model = Hall
 
