275207c370a222d03884a1e96980e19fcf2f897f
mandeeps708 <mandeeps708@gmail.com>
2015-02-12 18:49:14 +0530

    views, urls updated

--- src/bookingsystem/urls.py
+++ src/bookingsystem/urls.py
@@ -3,10 +3,10 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'bookingsystem.views.home', name='home'),
+    url(r'^$', 'home.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^view/', 'bookingsystem.views.view'),
-    url(r'^cancel/', 'bookingsystem.views.cancel'),
-    url(r'^book/', 'bookingsystem.views.book'),
+    url(r'^view/', 'home.views.view'),
+    url(r'^cancel/', 'home.views.cancel'),
+    url(r'^book/', 'home.views.book'),
     url(r'^admin/', include(admin.site.urls)),
 )
--- src/home/views.py
+++ src/home/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
-from .forms import BookingForm
+# from .forms import BookingForm
+
+# def home(request):
+# 		form = BookingForm()
+# 		context = {"form": form}
+# 		template = "home.html"
+# 		return render(request, template, context)
 
 def home(request):
-		form = BookingForm()
-		context = {"form": form}
-		template = "home.html"
-		return render(request, template, context)
+		return render(request, "home.html", {})
+
+def book(request):
+		return render(request, "book.html", {})
+
+def view(request):
+		return render(request, "view.html", {})
+
+def cancel(request):
+		return render(request, "cancel.html", {})
