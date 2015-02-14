fc48cd1cc9b41ff953bd265236a040f4cfc3f848
mandeeps708 <mandeeps708@gmail.com>
2015-02-14 15:56:35 +0530

    working booking

--- src/home/forms.py
+++ src/home/forms.py
@@ -1,4 +1,20 @@
 from django import forms
+# from django.forms import ModelForm
+# from .models import *
+# from django.contrib.admin.widgets import AdminDateWidget 
+from .models import Booking
 
-class BookingForm(forms.Form)
-	hall = forms.CharField()
+class BookingForm(forms.ModelForm):
+	class Meta:
+		model = Booking 
+		fields = ["hall", "date", "start_time", "end_time", "event_name", "name", "email"]
+
+	# hall = forms.CharField()
+	# hall = forms.CharField()
+	# email = forms.EmailField()
+	# date = forms.DateField()
+
+	# date = forms.DateField(widget = AdminDateWidget)
+
+# class select(forms.Form):
+#     hall = forms.ModelChoiceField(queryset=hall.objects.all().order_by('name'))
--- src/home/views.py
+++ src/home/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
-# from .forms import BookingForm
+from .forms import BookingForm
+from .models import Booking
 
 # def home(request):
 # 		form = BookingForm()
@@ -13,7 +14,25 @@ def home(request):
 		return render(request, "home.html", {})
 
 def book(request):
-		return render(request, "book.html", {})
+
+		form = BookingForm(request.POST or None)
+
+		if form.is_valid():
+			save_it = form.save(commit=False)
+			save_it.save()
+		# print request.POST["email"], request.POST["email_2"]
+		# form = BookingForm(request.POST or None)
+		# if form.is_valid():
+		# 	email = form.cleaned_data['email']
+		# 	hall = form.cleaned_data['hall']
+		# 	date = form.cleaned_data['date']
+		# 	email_join, created = Booking.objects.get_or_create(email=email)
+		# 	hall_entry, created3 = Booking.objects.get_or_create(hall=hall)
+		# 	date, created2 = Booking.objects.get_or_create(date=date)	
+		# 	print hall_entry, created3, email_join, created, date, created2
+
+		context = {"form": form}
+		return render(request, "book.html", context)
 
 def view(request):
 		return render(request, "view.html", {})
