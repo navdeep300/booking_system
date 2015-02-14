33be2f3e223fdcdc4e7895a604c83c7dc8109672
mandeeps708 <mandeeps708@gmail.com>
2015-02-15 02:20:18 +0530

    booking override restriction

--- src/home/views.py
+++ src/home/views.py
@@ -11,15 +11,15 @@ from .models import Booking
 # 		template = "home.html"
 # 		return render(request, template, context)
 def home(request):
-		return render(request, "home.html", {})
+	return render(request, "home.html", {})
 
 def book(request):
 
-		form = BookingForm(request.POST or None)
+	form = BookingForm(request.POST or None)
 
-		if form.is_valid():
-			save_it = form.save(commit=False)
-			save_it.save()
+	if form.is_valid():
+		save_it = form.save(commit=False)
+		save_it.save()
 		# print request.POST["email"], request.POST["email_2"]
 		# form = BookingForm(request.POST or None)
 		# if form.is_valid():
@@ -31,14 +31,15 @@ def book(request):
 		# 	date, created2 = Booking.objects.get_or_create(date=date)	
 		# 	print hall_entry, created3, email_join, created, date, created2
 
-		context = {"form": form}
-		return render(request, "book.html", context)
+	context = {"form": form}
+	return render(request, "book.html", context)
 
 def view(request):
-		return render(request, "view.html", {})
+
+	return render(request, "view.html", {})
 
 def cancel(request):
-		return render(request, "cancel.html", {})
+	return render(request, "cancel.html", {})
 
 
 def login_view(request):
